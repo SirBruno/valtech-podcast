e2ae7c482d0938974e1d731a1cc2997464c133c2
sirbruno <bruno.webjr@gmail.com>
2022-04-19 18:04:32 -0300

    Section 3.

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,9 @@ import ValtechLogoBlack from '../public/valtech_logo_black.png'
 import epAvatar1 from '../public/epAvatar1.png'
 import rectangleLines from '../public/rectangle_lines.svg'
 import playCircleFilledWhite from '../public/play_circle_filled_white.svg'
+import person1 from '../public/person1.png'
 import oval from '../public/oval.svg'
+import oval2 from '../public/oval2.png'
 
 export default function Home() {
   return (
@@ -114,6 +116,22 @@ export default function Home() {
             </div>
           </div>
           <a className={styles.outlinedBtn}>View more</a>
+        </section>
+      </section>
+      <section className={styles.sectionThree}>
+        <section>
+          <div>
+            <div className={styles.oval2} style={{ width: '750px', height: '150%' }}>
+              <Image src={oval2} alt="design element" quality="100" layout='fill' objectFit='contain' />
+            </div>
+            <span className={styles.backgroundTitle}>About<br />the</span>
+            <h2>about the show</h2>
+            <span className={styles.blueLine}></span>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p>
+          </div>
+          <div style={{ width: '500px', height: '100%', position: 'relative' }}>
+            <Image className={styles.person1} src={person1} alt="Young man" quality="100" layout='fill' objectFit='contain' />
+          </div>
         </section>
       </section>
     </>
