3de6f010b5bb5a8a980d839a58502fcce028b669
sirbruno <bruno.webjr@gmail.com>
2022-04-20 18:21:08 -0300

    Responsive v1

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,28 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { faComputerMouse, faAngleDown, faBars, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import ValtechLogoBlack from '../public/valtech_logo_black.png'
 import ValtechLogoWhite from '../public/ValtechLogoWhite.png'
 import epAvatar1 from '../public/epAvatar1.png'
 import rectangleLines from '../public/rectangle_lines.svg'
 import playCircleFilledWhite from '../public/play_circle_filled_white.svg'
 import person1 from '../public/person1.png'
 import person2 from '../public/person2.png'
-import oval from '../public/oval.svg'
 import oval2 from '../public/oval2.png'
 import oval3 from '../public/oval3.png'
 import oval4 from '../public/oval4.png'
+import oval5 from '../public/oval5.svg'
+import SectionOne from './Components/SectionOne.js'
 
 export default function Home() {
   return (
     <>
-      <div className={styles.container}>
-        <Head>
-          <title>Valtech Podcast</title>
-        </Head>
-        <FontAwesomeIcon className={styles.menuBtn} icon={faBars} />
-        <main className={styles.main}>
-          <span className={styles.backgroundTitle}>Podcast</span>
-          <div className={styles.rectangleLines}>
-            <Image src={rectangleLines} alt="design element" quality="100" layout='fill' objectFit='contain' />
-          </div>
-          <div className={styles.oval}>
-            <Image src={oval} alt="design element" quality="100" layout='fill' objectFit='contain' />
-          </div>
-          <div>
-            <div style={{ width: '100%', height: '80px', position: 'relative' }}>
-              <Image className={styles.ValtechLogoBlack} src={ValtechLogoBlack} alt="Valtech logo" quality="100" layout='fill' objectFit='contain' />
-            </div>
-            <span className={styles.blueLine}></span>
-            <h1 className={styles.title}>The podcast</h1>
-            <a className={styles.blackBtn}>Listen now</a>
-          </div>
-        </main>
-        <div className={styles.scrollIcons}>
-          <FontAwesomeIcon icon={faComputerMouse} />
-          <FontAwesomeIcon icon={faAngleDown} />
-        </div>
-      </div>
+      <SectionOne />
       <section className={styles.sectionTwo}>
-        <span className={styles.backgroundTitle}>Last episodes</span>
+        <span className={styles.backgroundTitle}><span className={styles.desktopOnly}>Last </span>episodes</span>
         <span className={styles.blueCircle}></span>
         <section>
-          <h2 className={styles.title}>Last episodes</h2>
+          <h2 className={styles.title}>Last <br className={styles.mobileOnly} />episodes</h2>
           <span className={styles.blueLine}></span>
           <div className={styles.episodeFilter}>
             <a>All</a>
@@ -133,14 +106,14 @@ export default function Home() {
             <span className={styles.blueLine}></span>
             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p>
           </div>
-          <div style={{ width: '550px', height: '100%', position: 'relative' }}>
+          <div className={styles.person1}>
             <Image className={styles.person1} src={person1} alt="Young man" quality="100" layout='fill' objectFit='contain' />
           </div>
         </section>
       </section>
       <section className={styles.sectionFour}>
         <section>
-          <div style={{ width: '550px', height: '100%', position: 'relative' }}>
+          <div className={styles.person2}>
             <Image className={styles.person2} src={person2} alt="Young woman" quality="100" layout='fill' objectFit='contain' />
             <div>
               <p>Fake company CTO</p>
@@ -160,6 +133,11 @@ export default function Home() {
             <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.</p>
             <div className={styles.oval4}>
               <Image src={oval4} alt="design element" quality="100" layout='fill' objectFit='contain' />
+            </div>
+            <div className={styles.mobileOnly}>
+              <div className={styles.oval5}>
+                <Image src={oval5} alt="design element" quality="100" layout='fill' objectFit='contain' />
+              </div>
             </div>
             <div className={styles.rectangleLines}>
               <Image src={rectangleLines} alt="design element" quality="100" layout='fill' objectFit='contain' />
@@ -191,7 +169,7 @@ export default function Home() {
             <a>Episodes</a>
             <a>Contact</a>
           </div>
-          <p>©2022 Valtech. All rights reserved.</p>
+          <p className={styles.copyrightText}>©2022 Valtech. All rights reserved.</p>
         </section>
       </section>
     </>
