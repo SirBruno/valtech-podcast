d4b705ed855fa921d94f33633f986f619522112f
sirbruno <bruno.webjr@gmail.com>
2022-04-19 21:46:42 -0300

    Section 5.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { faComputerMouse, faAngleDown, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faComputerMouse, faAngleDown, faBars, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import ValtechLogoBlack from '../public/valtech_logo_black.png'
+import ValtechLogoWhite from '../public/ValtechLogoWhite.png'
 import epAvatar1 from '../public/epAvatar1.png'
 import rectangleLines from '../public/rectangle_lines.svg'
 import playCircleFilledWhite from '../public/play_circle_filled_white.svg'
@@ -164,6 +165,33 @@ export default function Home() {
               <Image src={rectangleLines} alt="design element" quality="100" layout='fill' objectFit='contain' />
             </div>
           </div>
+        </section>
+      </section>
+      <section className={styles.sectionFive}>
+        <section>
+          <span className={styles.backgroundTitle}>Subscribe</span>
+          <h2>Subscribe</h2>
+          <span className={styles.blueLine}></span>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br />Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</p>
+          <div className={styles.subscribeInput}>
+            <FontAwesomeIcon className={styles.faEnvelope} icon={faEnvelope} />
+            <input type="email" placeholder="Type your email" />
+            <button>send</button>
+          </div>
+          <br />
+          <br />
+          <span className={styles.divider}></span>
+          <div style={{ width: '100%', height: '40px', position: 'relative' }}>
+            <Image className={styles.ValtechLogoWhite} src={ValtechLogoWhite} alt="Young man" quality="100" layout='fill' objectFit='contain' />
+          </div>
+          <span className={styles.blueLine}></span>
+          <div className={styles.episodeFilter}>
+            <a>Home</a>
+            <a>The show</a>
+            <a>Episodes</a>
+            <a>Contact</a>
+          </div>
+          <p>©2022 Valtech. All rights reserved.</p>
         </section>
       </section>
     </>
